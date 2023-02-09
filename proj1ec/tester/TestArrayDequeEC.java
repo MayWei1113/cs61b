b6d592f7b7999632e14ecdb6ff041a9639fc1673
MayWei1113 <83398360+MayWei1113@users.noreply.github.com>
2023-02-09 11:09:45 +0800

    delete size assert

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -5,20 +5,31 @@ import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import student.StudentArrayDeque;
 
+import java.lang.reflect.Array;
+
 public class TestArrayDequeEC {
 
-    private static String errorMessage(String[] s, int j) {
-        String errMes = "\n";
+    private String errorMessage(String[] s, int j) {
+        String errMes = "";
         for (int oind = j - 2; oind <= j; oind += 1) {
             if (oind < 0) {
                 continue;
             }
-            errMes = errMes + s[oind] + '\n';
+            errMes = errMes + '\n' + s[oind];
         }
         return errMes;
     }
+
+    public String[] checkSize(ArrayDequeSolution L, StudentArrayDeque B, String[] errMessages, int j) {
+        int corrSize = L.size();
+        int stuSize = B.size();
+        errMessages[j] = "size()";
+        String errmess = errorMessage(errMessages, j);
+        assertEquals(errmess, corrSize, stuSize);
+        return errMessages;
+    }
     @Test
-    public static void randomisedTest() {
+    public void randomisedTest() {
         ArrayDequeSolution<Integer> L = new ArrayDequeSolution<>();
         StudentArrayDeque<Integer> B = new StudentArrayDeque<>();
 
@@ -32,44 +43,36 @@ public class TestArrayDequeEC {
                 int randVal = StdRandom.uniform(0, 100);
                 L.addLast(randVal);
                 B.addLast(randVal);
-                int size = L.size();
-                int bSize = B.size();
                 output[j] = "addLast(" + randVal + ")";
-                String errmes = errorMessage(output, j);
-                j = j + 1;
-                assertEquals(errmes, size, bSize);
+                j += 1;
             } else if (operationNumber == 1) {
                 // size
                 int randVal = StdRandom.uniform(0, 100);
                 L.addFirst(randVal);
                 B.addFirst(randVal);
-                int size = L.size();
-                int bSize = B.size();
                 output[j] = "addFirst(" + randVal + ")";
-                String errmes = errorMessage(output, j);
-                j = j + 1;
-                assertEquals(errmes, size, bSize);
+                j += 1;
             } else if (operationNumber == 2 & L.size() > 0) {
                 // remove last
                 Integer x = L.removeLast();
                 Integer bX = B.removeLast();
                 output[j] = "removeLast()";
                 String errmes = errorMessage(output, j);
-                j = j + 1;
                 assertEquals(errmes, x, bX);
+                j += 1;
             } else if (operationNumber == 3 & L.size() > 0) {
                 // remove first
                 Integer x = L.removeFirst();
                 Integer bX = B.removeFirst();
                 output[j] = "removeFirst()";
                 String errmes = errorMessage(output, j);
-                j = j + 1;
                 assertEquals(errmes, x, bX);
+                j += 1;
             }
         }
     }
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         randomisedTest();
     }
 }
